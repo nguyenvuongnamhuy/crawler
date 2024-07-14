a374fd179b4a98895f06a30685d4812827f7648f
Huy Nguyen <nguyenvuongnamhuy@gmail.com>
2024-07-14 23:55:06 +0700

    add documents

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# crawler-nodejs
+## Description
+
+This is a Crawling Data Application using NodeJS and Cheerio
+
+Source: https://github.com/imphunq/crawler-nodejs
+
+## Install
+
+```bash
+npm install
+```
+
+## Run
+
+```bash
+npm run start
+```
+
+---
--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const cheerio = require('cheerio');
-const request = require('request-promise');
-const fs = require('fs');
+const cheerio = require("cheerio");
+const request = require("request-promise");
+const fs = require("fs");
 
-request('https://123job.vn/tuyen-dung', (error, response, html) => {
-  if(!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-    let data = []
-    $('.job__list-item').each((index, el) => {
-      const job = $(el).find('.job__list-item-title a').text();
-      const company = $(el).find('.job__list-item-company span').text();
-      const address = $(el).find('.job__list-item-info').find('.address').text();
-      const salary = $(el).find('.job__list-item-info').find('.salary').text();
+const HOST = "https://123job.vn/tuyen-dung";
 
-      data.push({
-        job, company, address, salary
-      });
+request(HOST, (error, response, html) => {
+  if (error && response.statusCode !== 200) {
+    throw new Error(error);
+  }
+
+  console.log(`> Crawling data from ${HOST}`);
+
+  const data = [];
+  const $ = cheerio.load(html);
+
+  $(".job__list-item").each((_index, el) => {
+    const job = $(el).find(".job__list-item-title a").text();
+    const company = $(el).find(".job__list-item-company span").text();
+    const address = $(el).find(".job__list-item-info").find(".address").text();
+    const salary = $(el).find(".job__list-item-info").find(".salary").text();
+
+    data.push({
+      job,
+      company,
+      address,
+      salary,
     });
+  });
 
-    fs.writeFileSync('data.json', JSON.stringify(data));
-  }
-  else {
-    console.log(error);
-  }
+  fs.writeFileSync("data.json", JSON.stringify(data));
+
+  console.log("> Finish");
 });
